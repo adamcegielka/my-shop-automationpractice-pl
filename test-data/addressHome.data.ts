c0d28ad492bf564edf4f156082e089496580e1fe
Adam <adamcegielka@tlen.pl>
2023-06-26 13:00:36 +0200

    feat: add addressHomeData

---
+++ test-data/addressHome.data.ts
@@ -0,0 +1,12 @@
+export const addressHomeData = {
+  addressTitle: 'Home address', 
+  addressLine1: '29 Cove Lane',
+  addressLine2: 'Arizona 95924',
+  city: 'Phoenix',
+  state: 'Arizona',
+  zip: '95924',
+  country: 'United States',
+  phonNumber: '4680211945',
+  mobileNumber: '436943561',
+  additionalInformation: 'Please leave in the letterbox',
+};
