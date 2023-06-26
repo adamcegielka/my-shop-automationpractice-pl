70bcf744244230a9870df8ea3f537170ba27838a
Adam <adamcegielka@tlen.pl>
2023-06-26 13:06:13 +0200

    feat: add addressWorkData

---
+++ test-data/addressWork.data.ts
@@ -0,0 +1,12 @@
+export const addressWorkData = {
+  addressTitle: 'Work address', 
+  addressLine1: '106 Stillwell Avenue APT 34',
+  addressLine2: 'San Francisco 79255',
+  city: 'San Francisco',
+  state: 'California',
+  zip: '79255',
+  country: 'United States',
+  phonNumber: '808068325',
+  mobileNumber: '436943561',
+  additionalInformation: 'Please leave to the secretary',
+};
