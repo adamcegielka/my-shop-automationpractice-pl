71b325773a0b0d3e4f0491eb133ad731132f20c4
Adam <adamcegielka@tlen.pl>
2023-07-30 17:59:41 +0200

    fead(test): add test `008-TC` with random email

--- tests/e2e/registration.spec.ts
+++ tests/e2e/registration.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { Registration } from '../../pages/registration.page';
 import { registrationData } from '../../test-data/registration.data';
+import { getRandomEmail } from '../../utils/data-helpers';
 
 test.describe('Registration: verification email field for account creation', () => {
   let registration: Registration;
@@ -17,8 +18,8 @@ test.describe('Registration: verification email field for account creation', () 
     const validationEmailField = 'Invalid email address.';
 
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
   test('001-TC validation e-mail address with specs at beginning', async ({ page }) => {
@@ -28,8 +29,8 @@ test.describe('Registration: verification email field for account creation', () 
 
     await registration.inputEmail.fill(emailWithSpace);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
   test('002-TC verification e-mail address with dot at beginning', async ({ page }) => {
@@ -39,8 +40,8 @@ test.describe('Registration: verification email field for account creation', () 
 
     await registration.inputEmail.fill(emailWithDot);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
   test('003-TC verification e-mail address with hyphen at beginning', async ({ page }) => {
@@ -50,8 +51,8 @@ test.describe('Registration: verification email field for account creation', () 
 
     await registration.inputEmail.fill(emailWithDash);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
   test('004-TC verification e-mail address with underscore at beginning', async ({ page }) => {
@@ -61,40 +62,51 @@ test.describe('Registration: verification email field for account creation', () 
 
     await registration.inputEmail.fill(emailWithUnderscore);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
-  test('005-TC verificatione-mail address with special apostrophe sign', async ({ page }) => {
+  test('005-TC verification e-mail address with special apostrophe sign', async ({ page }) => {
     registration = new Registration(page);
     const emailWitApostrof = registrationData.userEmailWithApostropheSign;
     const validationEmailField = 'Invalid email address.';
 
     await registration.inputEmail.fill(emailWitApostrof);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
-  test('006-TC verificatione-mail address with special equation sign', async ({ page }) => {
+  test('006-TC verificatione e-mail address with special equation sign', async ({ page }) => {
     registration = new Registration(page);
     const emailWitEquation = registrationData.userEmailWithEquationSign;
     const validationEmailField = 'Invalid email address.';
 
     await registration.inputEmail.fill(emailWitEquation);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
   });
 
-  test('007-TC verificatione-mail address with special ampersand sign', async ({ page }) => {
+  test('007-TC verificatione e-mail address with special ampersand sign', async ({ page }) => {
     registration = new Registration(page);
     const emailWitAmpersand = registrationData.userEmailWithAmpersandSign;
     const validationEmailField = 'Invalid email address.';
 
     await registration.inputEmail.fill(emailWitAmpersand);
     await registration.bottonCreateAccount.click();
-    const EmailFieldValidation = registration.validationEmail;
-    await expect(EmailFieldValidation).toContainText(validationEmailField);
+    const emailFieldValidation = registration.validationEmail;
+    await expect(emailFieldValidation).toContainText(validationEmailField);
+  });
+
+  test('008-TC verificatione correct e-mail address', async ({ page }) => {
+    registration = new Registration(page);
+    const emailRandom = await getRandomEmail();
+    const validationAuthentication = 'Your personal information';
+
+    await registration.inputEmail.fill(emailRandom);
+    await registration.bottonCreateAccount.click();
+    const pageAuthentication = registration.validationPageAuthentication;
+    await expect(pageAuthentication).toContainText(validationAuthentication);
   });
 });
