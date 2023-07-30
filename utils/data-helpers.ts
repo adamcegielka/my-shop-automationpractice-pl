ab5401661a423cfcc4380570f12ba8e32b239762
Adam <adamcegielka@tlen.pl>
2023-07-30 17:40:37 +0200

    test: add random email address generator

--- utils/data-helpers.ts
+++ utils/data-helpers.ts
@@ -1,9 +1,21 @@
-// const crypto = require('crypto');
+export async function getRandomEmail() {
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+  const usernameLength = 8;
+  const domainLength = 5;
 
-// export async function getRandomNumber() {
-//   return Math.floor(Math.random() * 10000 + 1);
-// }
+  // Randomly generate user's name
+  let username = '';
+  for (let i = 0; i < usernameLength; i++) {
+    username += letters.charAt(Math.floor(Math.random() * letters.length));
+  }
 
-// export async function getRandomString() {
-//   return crypto.randomBytes(20).toString('hex');
-// }
+  // Random generate email domain
+  let domain = '';
+  for (let i = 0; i < domainLength; i++) {
+    domain += letters.charAt(Math.floor(Math.random() * letters.length));
+  }
+
+  // Construct a full e-mail address
+  const email = `${username}@${domain}.com`;
+  return email;
+}
