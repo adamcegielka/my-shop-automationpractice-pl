7bf98fa1cda7a84ff4716efb095bf5a8e6022142
Adam <adamcegielka@tlen.pl>
2023-08-03 13:30:12 +0200

    tests: change in testDir

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: './tests',
   // Each test is given 30 seconds.
   timeout: 30000,
   /* Run tests in files in parallel */
