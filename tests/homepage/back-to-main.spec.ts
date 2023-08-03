78e4bc0284f18fe660ef0faf5d62b874c43f5a03
Adam <adamcegielka@tlen.pl>
2023-08-03 14:33:23 +0200

    feat(test): add test `go back to homepage from Sign in page`

--- tests/homepage/back-to-main.spec.ts
+++ tests/homepage/back-to-main.spec.ts
@@ -5,11 +5,19 @@ test.describe('Go back to the main page', () => {
     await page.goto('/');
   });
 
-  test('go back to homepage from contact us page', async ({ page }) => {
-    const contactbutton = await page.locator('#contact-link');
-    await contactbutton.click();
+  test('go back to homepage from Contact us page', async ({ page }) => {
+    const contactButton = await page.locator('#contact-link');
+    await contactButton.click();
     await expect(page).toHaveTitle(/Contact us - My Shop/);
-    await page.locator('a[title="My Shop"]');
+    await page.locator('a[title="My Shop"]').click();
+    await expect(page).toHaveTitle(/My Shop/);
+  });
+
+  test('go back to homepage from Sign in page', async ({ page }) => {
+    const signInButton = await page.locator('.login');
+    await signInButton.click();
+    await expect(page).toHaveTitle(/Login - My Shop/);
+    await page.locator('#header_logo').click();
     await expect(page).toHaveTitle(/My Shop/);
   });
 });
