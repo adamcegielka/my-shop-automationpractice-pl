8729c13e02ff46921b655a83d2ab3a7424231d80
Adam <adamcegielka@tlen.pl>
2023-08-03 15:42:56 +0200

    feat(test): add test `go back to homepage from Dresses page`

--- tests/homepage/nav-top-menu.spec.ts
+++ tests/homepage/nav-top-menu.spec.ts
@@ -1,15 +1,24 @@
 import { expect, test } from '@playwright/test';
 
-test.describe.only('Go back to homepage by top menu', () => {
+test.describe('Go back to homepage by top menu', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   });
 
-  test.only('go back to homepage from Women page', async ({ page }) => {
-    const menuNavigation = await page.locator('a[title="Women"]');
-    await menuNavigation.click();
-    await expect(page).toHaveTitle(/Women - My Shop/);
+  test.afterEach(async ({ page }) => {
     await page.locator('a[title="My Shop"]');
     await expect(page).toHaveTitle(/My Shop/);
+  })
+
+  test('go back to homepage from Women page', async ({ page }) => {
+    const womenButton = await page.locator('a[title="Women"]');
+    await womenButton.click();
+    await expect(page).toHaveTitle(/Women - My Shop/);
+  });
+
+  test('go back to homepage from Dresses page', async ({ page }) => {
+    const dressesButton = await page.getByRole('link', { name: 'Dresses', exact: true });
+    await dressesButton.click();
+    await expect(page.locator('#categories_block_left')).toBeVisible();
   });
 });
