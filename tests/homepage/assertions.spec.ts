ca2bdcf99c77cd3960d5d5bb88f6b1d6818b5d89
Adam <adamcegielka@tlen.pl>
2023-08-03 12:26:10 +0200

    feat(test): add `asserts homepage`

---
+++ tests/homepage/assertions.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('asserts homepage', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle(/My Shop/);
+  await expect(page).toHaveURL('http://www.automationpractice.pl/index.php');
+  await expect(page.locator('#search_query_top')).toBeEmpty();
+  await expect(page.locator('.shopping_cart')).toBeVisible();
+  await expect(page.getByText('Custom Block')).toBeVisible();
+  await expect(page.locator('#facebook_block')).toHaveText('Follow us on Facebook');
+  await expect(page.locator('.tab-content')).toContainText('No featured products');
+  await expect(page.locator('.bottom-footer')).toHaveCount(1);
+  await expect(page.locator('.footer-container')).toBeTruthy();
+  await expect(page.locator('#user_email')).toHaveAttribute('type', 'text');
+  await expect(page.locator('#contact-link')).toBeEnabled();
+  await expect(page.locator('.icon-truck')).toHaveClass('font-family');
+  await expect(page.locator('#newsletter-input')).toHaveValue('Enter your e-mail');
+});
