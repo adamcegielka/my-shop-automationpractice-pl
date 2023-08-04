3a1f43c9df2e1e528a7dfad2efc4872f57dcb7c3
Adam <adamcegielka@tlen.pl>
2023-08-04 14:18:32 +0200

    tests: refactor tests

--- tests/e2e/registration-form.spec.ts
+++ tests/e2e/registration-form.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import { Registration } from 'pages/registration.page';
 import { registrationData } from 'test-data/registration.data';
 
@@ -8,8 +8,6 @@ test.describe('Testing new user registration form', () => {
   const firstName = registrationData.userFirstNameValid;
   const lastName = registrationData.userLastNameValid;
   const password = registrationData.userPasswordValid;
-  const twoErrorMessage = registrationData.twoErrorMessage;
-  const oneErrorMessage = registrationData.oneErrorMessage;
 
   test.beforeEach(async ({ page }) => {
     registration = new Registration(page);
@@ -20,67 +18,57 @@ test.describe('Testing new user registration form', () => {
   });
 
   test('009-TC validation of registration form', async () => {
-    const errorMessage = registrationData.threeErrorMessage;
-
     await registration.clickOnRegister();
-    const threeMessageValidation = registration.alertErrorMessage;
-    await expect(threeMessageValidation).toContainText(errorMessage);
+    await registration.asserThreeErrorMessage();
   });
 
   test('010-TC verification of new user registration with first name only', async () => {
     await registration.inputFirstName.fill(firstName);
     await registration.clickOnRegister();
-    const twoMessageValidation = registration.alertErrorMessage;
-    await expect(twoMessageValidation).toContainText(twoErrorMessage);
+    await registration.asserTwoErrorMessage();
   });
 
   test('011-TC erification of new user registration with last name only', async () => {
     await registration.inputLastName.fill(lastName);
     await registration.clickOnRegister();
-    const twoMessageValidation = registration.alertErrorMessage;
-    await expect(twoMessageValidation).toContainText(twoErrorMessage);
+    await registration.asserTwoErrorMessage();
   });
 
   test('012-TC verification of new user registration with password only', async ({ page }) => {
     await page.waitForTimeout(1000);
     await registration.inputPassword.fill(password);
     await registration.clickOnRegister();
-    const twoMessageValidation = registration.alertErrorMessage;
-    await expect(twoMessageValidation).toContainText(twoErrorMessage);
+    await registration.asserTwoErrorMessage();
   });
 
   test('013-TC verification of new user registration without first name required', async () => {
     await registration.inputLastName.fill(lastName);
     await registration.inputPassword.fill(password);
     await registration.clickOnRegister();
-    const oneMessageValidation = registration.alertErrorMessage;
-    await expect(oneMessageValidation).toContainText(oneErrorMessage);
+    await registration.asserOneErrorMessage();
   });
 
   test('014-TC verification of new user registration without last name required', async () => {
     await registration.inputFirstName.fill(firstName);
     await registration.inputPassword.fill(password);
     await registration.clickOnRegister();
-    const oneMessageValidation = registration.alertErrorMessage;
-    await expect(oneMessageValidation).toContainText(oneErrorMessage);
+    await registration.asserOneErrorMessage();
   });
 
   test('015-TC verification of new user registration without password required', async () => {
     await registration.inputFirstName.fill(firstName);
     await registration.inputLastName.fill(lastName);
     await registration.clickOnRegister();
-    const oneMessageValidation = registration.alertErrorMessage;
-    await expect(oneMessageValidation).toContainText(oneErrorMessage);
+    await registration.asserOneErrorMessage();
   });
 
   test('016-TC verification of new user registration with 4 characters in password field', async () => {
     const shortPassword = registrationData.userPasswordShort;
-
+    
     await registration.inputFirstName.fill(firstName);
     await registration.inputLastName.fill(lastName);
     await registration.inputPassword.fill(shortPassword);
     await registration.clickOnRegister();
-    const oneMessageValidation = registration.alertErrorMessage;
-    await expect(oneMessageValidation).toContainText(oneErrorMessage);
+    await registration.asserOneErrorMessage();
   });
 });
