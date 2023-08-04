5aac6269a53619d5b894538eed852e18ab752f21
Adam <adamcegielka@tlen.pl>
2023-08-04 13:24:49 +0200

    tests: refactor tests

--- tests/e2e/registration-form.spec.ts
+++ tests/e2e/registration-form.spec.ts
@@ -4,58 +4,51 @@ import { registrationData } from 'test-data/registration.data';
 // import { getRandomName } from '@utils/date-names';
 // import { getRandomSurname } from '@utils/date-names';
 
-test.describe.only('Testing new user registration form', () => {
+test.describe('Testing new user registration form', () => {
   let registration: Registration;
   const email = registrationData.userEmailValid;
-  const errorMessage = registrationData.twoErrorMessage;
+  const firstName = registrationData.userFirstNameValid;
+  const lastName = registrationData.userLastNameValid;
+  const password = registrationData.userPasswordValid;
+  const twoErrorMessage = registrationData.twoErrorMessage;
+  const oneErrorMessage = registrationData.oneErrorMessage;
+
 
   test.beforeEach(async ({ page }) => {
     registration = new Registration(page);
     await page.goto('/');
     await registration.clickOnSignIn();
+    await registration.inputEmail.fill(email);
+    await registration.clickOnCreateAccount();
   });
 
   test('009-TC validation of registration form', async () => {
     const errorMessage = registrationData.threeErrorMessage;
 
-    await registration.inputEmail.fill(email);
-    await registration.clickOnCreateAccount();
     await registration.clickOnRegister();
     const threeMessageValidation = registration.alertErrorMessage;
     await expect(threeMessageValidation).toContainText(errorMessage);
   });
 
   test('010-TC verification of new user registration with first name only', async () => {
-    const firstName = registrationData.userFirstNameValid;
-
-    await registration.inputEmail.fill(email);
-    await registration.clickOnCreateAccount();
     await registration.inputFirstName.fill(firstName);
     await registration.clickOnRegister();
     const twoMessageValidation = registration.alertErrorMessage;
-    await expect(twoMessageValidation).toContainText(errorMessage);
+    await expect(twoMessageValidation).toContainText(twoErrorMessage);
   });
 
   test('011-TC erification of new user registration with last name only', async () => {
-    const lastName = registrationData.userLastNameValid;
-
-    await registration.inputEmail.fill(email);
-    await registration.clickOnCreateAccount();
     await registration.inputLastName.fill(lastName);
     await registration.clickOnRegister();
     const twoMessageValidation = registration.alertErrorMessage;
-    await expect(twoMessageValidation).toContainText(errorMessage);
+    await expect(twoMessageValidation).toContainText(twoErrorMessage);
   });
 
   test('012-TC verification of new user registration with password only', async ({ page }) => {
-    const password = registrationData.userPasswordValid;
-
-    await registration.inputEmail.fill(email);
-    await registration.clickOnCreateAccount();
     await page.waitForTimeout(1000);
     await registration.inputPassword.fill(password);
     await registration.clickOnRegister();
     const twoMessageValidation = registration.alertErrorMessage;
-    await expect(twoMessageValidation).toContainText(errorMessage);
+    await expect(twoMessageValidation).toContainText(twoErrorMessage);
   });
 });
