1bad3bcd05bd4b67605e6127587baa5c61eff3c4
Adam <adamcegielka@tlen.pl>
2023-08-05 15:02:22 +0200

    tests: refactor tests

--- tests/e2e/registration-form-random.spec.ts
+++ tests/e2e/registration-form-random.spec.ts
@@ -6,8 +6,11 @@ import { getRandomFirstName } from '@utils/date-user';
 import { getRandomLastName } from '@utils/date-user';
 import { getRandomPassword } from '@utils/date-user';
 
-test.describe('Testing a new user registration form with random data @failTest', () => {
+test.describe('Testing a new user registration form with random data', () => {
   let registration: Registration;
+  let randomFirstName: string;
+  let randomLastName: string;
+  let randomPassword: string;
 
   test.beforeEach(async ({ page }) => {
     registration = new Registration(page);
@@ -17,13 +20,14 @@ test.describe('Testing a new user registration form with random data @failTest',
     await registration.clickOnSignIn();
     await registration.inputEmail.fill(randomEmail);
     await registration.clickOnCreateAccount();
+
+    randomFirstName = await getRandomFirstName();
+    randomLastName = await getRandomLastName();
+    randomPassword = await getRandomPassword();
   });
 
-  test('017-TC verification of new user registration with one space in the first name field', async () => {
-    const randomLastName = await getRandomLastName();
-    const randomPassword = await getRandomPassword();
+  test('017-TC verification of new user registration with one space in the first name field @failTest', async () => {
     const oneSpace = registrationData.oneSpace;
-
     await registration.inputFirstName.fill(oneSpace);
     await registration.inputLastName.fill(randomLastName);
     await registration.inputPassword.fill(randomPassword);
@@ -31,15 +35,21 @@ test.describe('Testing a new user registration form with random data @failTest',
     await registration.asserOneErrorMessage();
   });
 
-  test('018-TC verification of new user registration with one space in last name field', async () => {
-    const randomFirstName = await getRandomFirstName();
-    const randomPassword = await getRandomPassword();
+  test('018-TC verification of new user registration with one space in last name field @failTest', async () => {
     const oneSpace = registrationData.oneSpace;
-
     await registration.inputFirstName.fill(randomFirstName);
     await registration.inputLastName.fill(oneSpace);
     await registration.inputPassword.fill(randomPassword);
     await registration.clickOnRegister();
     await registration.asserOneErrorMessage();
-  })
+  });
+
+  test('019-TC verification of new user registration with five spaces in the password field @failTest', async () => {
+    const fiveSpace = registrationData.fiveSpace;
+    await registration.inputFirstName.fill(randomFirstName);
+    await registration.inputLastName.fill(randomLastName);
+    await registration.inputPassword.fill(fiveSpace);
+    await registration.clickOnRegister();
+    await registration.asserOneErrorMessage();
+  });
 });
