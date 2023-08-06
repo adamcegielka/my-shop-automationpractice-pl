b1068a9057a5a64a931d329b31bb7372b5a93b90
Adam <adamcegielka@tlen.pl>
2023-08-06 18:49:06 +0200

    tests(feat): add functions in `data-dates.ts` file

---
+++ utils/data-dates.ts
@@ -0,0 +1,42 @@
+// Current Day Number
+export async function getCurrentDayNumber() {
+  const today = new Date();
+  const numberDay = today.getDate();
+
+  return numberDay;
+}
+
+// Number Day Next
+export async function getNumberDayNext() {
+  const today = new Date();
+  const currentDayNumber = today.getDate();
+
+  today.setDate(currentDayNumber + 1);
+  const numberDay = today.getDate();
+
+  return numberDay;
+}
+
+// Current Month
+export async function getCurrentMonth() {
+  const today = new Date();
+  const nameMonth = today.toLocaleString('en-US', { month: 'long' });
+
+  return nameMonth;
+}
+
+// Current Year
+export async function getCurrentYear() {
+  const today = new Date();
+  const year = today.toLocaleString(undefined, { year: 'numeric' });
+
+  return year;
+}
+
+// Year Birth Adult
+export async function getYearBirthAdult() {
+  const today = new Date();
+  const year = today.getFullYear() - 19;
+
+  return year;
+}
