fb9370d354ce76c5f539d3b0194cdc45c52e9b85
Adam <adamcegielka@tlen.pl>
2023-08-14 13:08:39 +0200

    tests(feat): add new test `022-TC`

--- tests/e2e/registration-form-date-birth.spec.ts
+++ tests/e2e/registration-form-date-birth.spec.ts
@@ -4,12 +4,18 @@ import { getRandomEmail } from '@utils/data-email';
 import { getRandomFirstName } from '@utils/date-user';
 import { getRandomLastName } from '@utils/date-user';
 import { getRandomPassword } from '@utils/date-user';
+import { getRandomDayNumber } from '@utils/data-dates';
+import { getRandomMonthNumber } from '@utils/data-dates';
+import { getYearBirthAdult } from '@utils/data-dates';
 
-test.describe('dd', () => {
+test.describe('Testing a new user registration form with random data and date of birth', () => {
   let registration: Registration;
   let randomFirstName: string;
   let randomLastName: string;
   let randomPassword: string;
+  let randomDay: string;
+  let randomMonth: string;
+  let yearBirthAdult: string;
 
   test.beforeEach(async ({ page }) => {
     registration = new Registration(page);
@@ -23,6 +29,9 @@ test.describe('dd', () => {
     randomFirstName = await getRandomFirstName();
     randomLastName = await getRandomLastName();
     randomPassword = await getRandomPassword();
+    randomDay = (await getRandomDayNumber()).toString();
+    randomMonth = (await getRandomMonthNumber()).toString();
+    yearBirthAdult = (await getYearBirthAdult()).toString();
   });
 
   test('021-TC verification of new user registration with only non required data', async () => {
@@ -33,5 +42,19 @@ test.describe('dd', () => {
     await registration.checkboxNewsletter.click();
     await registration.clickOnRegister();
     await registration.assertThreeErrorMessage();
+  });
+
+  test('022-TC verification of new user registration with all data', async () => {
+    await registration.titleMr.click();
+    await registration.inputFirstName.fill(randomFirstName);
+    await registration.inputLastName.fill(randomLastName);
+    await registration.inputPassword.fill(randomPassword);
+    await registration.dateOfBirthDays.selectOption(randomDay);
+    await registration.dateOfBirthMonths.selectOption(randomMonth);
+    await registration.dateOfBirthYears.selectOption(yearBirthAdult);
+    await registration.checkboxNewsletter.click();
+    await registration.clickOnRegister();
+    await registration.assertMyAccount();
+    await registration.assertAccountHasBeenCreated();
   });
 });
