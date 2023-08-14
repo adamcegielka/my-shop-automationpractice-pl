1be2cd733d28aa649e8f4fe13235d5238e277ce1
Adam <adamcegielka@tlen.pl>
2023-08-14 15:32:20 +0200

    tests(feat): add new tests `Users registration`

---
+++ tests/e2e/userRegistration.spec.ts
@@ -0,0 +1,27 @@
+import { test } from '@playwright/test';
+import { Registration } from 'pages/registration.page';
+
+test.describe('Users registration', () => {
+  let registration: Registration;
+
+  test.beforeEach(async ({ page }) => {
+    registration = new Registration(page);
+
+    await page.goto('/');
+    await registration.clickOnSignIn();
+  });
+
+  test('user first', async () => {
+    const userFirstEmail = 'user-first@example.com';
+    const firstName = 'User';
+    const lastName = 'First';
+    const password = 'abcdef';
+
+    await registration.inputEmail.fill(userFirstEmail);
+    await registration.clickOnCreateAccount();
+    await registration.inputFirstName.fill(firstName);
+    await registration.inputLastName.fill(lastName);
+    await registration.inputPassword.fill(password);
+    await registration.clickOnRegister();
+  });
+});
